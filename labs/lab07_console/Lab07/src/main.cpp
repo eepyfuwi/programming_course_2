662860d26f1d33c3384e95b790e76ef0c65808f1
eepyfuwi <donikplay@gmail.com>
2026-02-24 20:24:59 +0300

    7 лаба

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdio>
+
+using namespace std;
 
 // Lab 07
 // TODO: реализуйте решение по заданию в labs/lab07_console/README.md
@@ -10,22 +13,60 @@
 int main() {
     // Задание 1: вычисление значений функции.
     // Ввод: x0 — начальная точка интервала.
-    double x0 = 0.0;
-    if (!(std::cin >> x0)) {
-        return 0;
-    }
-    // dfdfdf
-    // // dfd/df
-    // /dfdfd
     // TODO: вычислите и выведите 10 значений функции на (0, 4].
-
+    double x0 = 0.0;
+    double x;
+    double l = 4.0 / 10;
+    for (int i = 0; i < 10; i++) {
+        x0 = x0 + l;
+        x = (sin(x0)) / x0;
+        cout << x << endl;
+    }
+    
+    cout << "-----------------------------------" << endl;
     // Задание 2: сумма чисел Фибоначчи.
     // Ввод: MAX — ограничение на сумму.
-    long long max_value = 0;
-    if (!(std::cin >> max_value)) {
-        return 0;
+    // TODO: вычислите N и S (S <= MAX) и выведите результаты.
+    long long max_value = 12000;
+    long long S = 0;
+    int N = 0;
+    long long F = 1;
+    long long F0 = 0;
+    long long F1 = 1;
+    while (S <= max_value) {
+        S = S + F;
+        F = F0 + F1;
+        F0 = F1;
+        F1 = F;
+        N++;
     }
 
-    // TODO: вычислите N и S (S <= MAX) и выведите результаты.
+    cout << S << " " << N << endl;
+    cout << "-----------------------------------" << endl;
+    // Задание 3: Кредит.
+    float m, p, sum, per, sumpay;
+    float sumper = 0;
+
+    scanf_s("%f %f %f", &sum, &p, &m);
+    cout << "-----------------------------------" << endl;
+    cout << "   Dolg" << " " << "    Procent" << " " << "   Platezh" << endl;
+    cout << "-----------------------------------" << endl;
+    float a = 1+ (p/100);
+    float mpay = sum / m;
+    while (sum > 0) {
+        per = sum * a - sum;
+        sumpay = mpay + per;
+        printf("%.2f %.2f %.2f\n", sum, per, sumpay);
+        sum = sum - mpay;
+        sumper = sumper + per;
+    }
+    cout << "-----------------------------------" << endl;
+    printf("Vsego procent: %.2f\n", sumper);
+    
+    
+    
+    
     return 0;
+
+    
 }
